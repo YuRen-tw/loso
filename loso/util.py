83100c98dc1149a6f6c4229683e81a67e62609ad
Yu-Ren Pan <YuRen.Pan.0117@gmail.com>
2019-05-15 14:07:23 +0800

    [2to3] util

--- loso/util.py
+++ loso/util.py
@@ -1,16 +1,16 @@
 def ngram(n, terms):
-    """An iterator for iterating n-gram terms from a text, for example:
+    '''An iterator for iterating n-gram terms from a text, for example:
     
     >>> list(ngram(2, ['Today', 'is', 'my', 'day']))
-    [['Today', 'is'], ['is', 'my'], ['my', 'day']] 
+    [['Today', 'is'], ['is', 'my'], ['my', 'day']]
     
     >>> list(ngram(3, ['Today', 'is', 'my', 'day']))
-    [['Today', 'is', 'my'], ['is', 'my', 'day']] 
+    [['Today', 'is', 'my'], ['is', 'my', 'day']]
     
-    """
-    for i in xrange(len(terms) - n + 1):
+    '''
+    for i in range(len(terms) - n + 1):
         yield terms[i:i+n]
         
-if __name__ == "__main__":
+if __name__ == '__main__':
     import doctest
     doctest.testmod()
